7868dacb2fb1caa77fb30757b01419f3be9d7381
hansenjonatann <hansenjonatann@gmail.com>
2025-01-09 20:39:26 +0700

    feat: change react-barcode to jsbarcode

--- components/barcode.tsx
+++ components/barcode.tsx
@@ -1,16 +1,19 @@
-'use client'
-import Barcode from "react-barcode";
+"use client";
+import { useRef } from "react";
+import JsBarcode from "jsbarcode";
 
-export default function CustomBarcode({value} : {value: string}) {
-return (
-    <>
-    <Barcode 
-    value={value}
-    format="CODE128"
-    width={2}
-    displayValue
-    height={50}
-    />
-    </>
-)
+export default function CustomBarcode({ value }: { value: string }) {
+  const barcodeRef = useRef<SVGSVGElement>(null);
+
+  // Langsung memanggil JsBarcode saat komponen dirender
+  if (barcodeRef.current) {
+    JsBarcode(barcodeRef.current, value, {
+      format: "CODE128", // Format barcode EAN-13
+      width: 2,
+      height: 50,
+      displayValue: true, // Menampilkan nilai di bawah barcode
+    });
+  }
+
+  return <svg ref={barcodeRef}></svg>;
 }
